96347c0364e571c8b272444bac631327d8a59e55
Stephen J. Eglen <sje30@cam.ac.uk>
2003-02-25 05:14:25 +0000

    Allow bin width to be a floating point value (e.g. 0.5um) rather than integer.
    New argument, *pauto, controls whether we are computing an
    autocorrelogram or not.  If we compute autocorrelogram, we should
    ignore i==j cases.  This avoids the need to test for very small
    values, which was always dubious for me.

--- src/sjedrp.c
+++ src/sjedrp.c
@@ -1,25 +1,19 @@
 #include <R.h>
 #include <S.h>			/* for seed_in, seed_out */
 
-#define SMALL_VALUE 0.00001
-
-/* If we are doing an AUTODRP, then data sets 1 and 2 are the same points.
- * In this case, when i==j the distance will be zero.  But we don't want
- * to include those points, so we reject very small distances.
- */
 
 /* Octave code was in ~/mosaics/code/drp */
 
 void drp_bin_it_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
 		  Sfloat *x2s, Sfloat *y2s, int *pn2,
-		  int *pnbins, int *pr,
+		  int *pnbins, Sfloat *pr, int *pauto,
 		  int   *ns);
 
 
 
 void drp_bin_it_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
 		  Sfloat *x2s, Sfloat *y2s, int *pn2,
-		  int *pnbins, int *pr,
+		  int *pnbins, Sfloat *pr, int *pauto,
 		  int   *ns)
 {
   /* First data set is (x1s, y1s) of length n1.  Second data set is
@@ -35,11 +29,12 @@ void drp_bin_it_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
    */
      
   int i,j;
-  Sfloat x1, y1, x2, y2, dx, dy, dist;
+  Sfloat x1, y1, x2, y2, dx, dy, dist, r;
   int bin;
 
-  int nbins, r, n1, n2;
-  nbins = *pnbins; n1 = *pn1; n2 = *pn2; r = *pr;
+  int nbins, n1, n2, do_auto;
+  nbins = *pnbins; n1 = *pn1; n2 = *pn2; r = *pr; do_auto=*pauto;
+  
   /*Rprintf("nbins is %d\n", nbins); */
 
   for(i=0; i<nbins; i++)
@@ -51,13 +46,15 @@ void drp_bin_it_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
     x1 = x1s[i]; y1 = y1s[i];
 
     for (j=0; j<n2; j++) {
-      x2 = x2s[j]; y2 = y2s[j];
-      dx = x1 - x2; dy = y1 - y2;
-      dist = sqrt( (double) ((dx*dx) + (dy*dy)));
-      bin = (int)(dist/r);
-      if ((bin < nbins) && (dist > SMALL_VALUE)) {
-	ns[bin]++;
-	/*if (bin == 0) printf("bin 0: distance %f i %d j %d\n", dist, i, j);*/
+      if (i==j) {
+	;
+      } else {
+	x2 = x2s[j]; y2 = y2s[j];
+	dx = x1 - x2; dy = y1 - y2;
+	dist = sqrt( (double) ((dx*dx) + (dy*dy)));
+	bin = (int)(dist/r);
+	if (bin < nbins)
+	  ns[bin]++;
       }
     }
   }
