44653cad5aacf8ea13a71e69c4efb35c9a3d9138
Stephen J. Eglen <sje30@cam.ac.uk>
2012-02-14 10:53:20 +0000

    (cross_corr_r): New function to return offsets.

--- src/sjedrp.c
+++ src/sjedrp.c
@@ -46,7 +46,7 @@ void drp_bin_it_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
     x1 = x1s[i]; y1 = y1s[i];
 
     for (j=0; j<n2; j++) {
-      if (i==j) {
+      if (do_auto && i==j) {
 	;
       } else {
 	x2 = x2s[j]; y2 = y2s[j];
@@ -58,5 +58,45 @@ void drp_bin_it_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
       }
     }
   }
+}
+
+void cross_corr_r(Sfloat *x1s, Sfloat *y1s, int *pn1,
+		  Sfloat *x2s, Sfloat *y2s, int *pn2,
+		  int *pnbins, Sfloat *pr, int *pauto,
+		  Sfloat *dxs,  Sfloat *dys, int *pk)
+
+{
+  /* First data set is (x1s, y1s) of length n1.  Second data set is
+   * (x2s, y2s) of length n2.  For each point in data set 1, we find
+   * the offset to each cell in data set 2.
+   */
+     
+  int i,j;
+  Sfloat x1, y1, x2, y2, dx, dy, dist2, r, maxdist;
+  int bin;
+
+  int nbins, n1, n2, do_auto, k;
+  nbins = *pnbins; n1 = *pn1; n2 = *pn2; r = *pr; do_auto=*pauto;
+  k = 0;
+  maxdist = (r*nbins)*(r*nbins);
+  
+  for(i=0; i<n1; i++) {
+    x1 = x1s[i]; y1 = y1s[i];
+
+    for (j=0; j<n2; j++) {
+      if (do_auto && (i==j)) {	/* TODO: check autocorrelation problem! */
+	;
+      } else {
+	x2 = x2s[j]; y2 = y2s[j];
+	dx = x1 - x2; dy = y1 - y2;
+	dist2 =  (double) ((dx*dx) + (dy*dy));
+	if (dist2 <= maxdist) {
+	  dxs[k] = dx; dys[k] = dy;
+	  k++;
+	}
+      }
+    }
+  }
+  *pk=k;
 }
 
