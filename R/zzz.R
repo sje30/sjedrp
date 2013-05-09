713a211bcc73278d6e3a00d460d2c412af4526bd
Stephen J. Eglen <sje30@cam.ac.uk>
2013-05-09 12:16:50 +0000

    Updated to 3.0.0 requirments.

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,6 @@
-.First.lib <- function(lib, pkg)
-  library.dynam("sjedrp", pkg, lib)
-
-.Last.lib <- function (libpath) {
+.onUnload <- function (libpath) {
   ## Run when the package is being unloaded.  This allows us to test
   ## packages within same session when dynlib is updated.
+  ## Detach package using: unloadNamespace("sjedrp")
   library.dynam.unload("sjedrp", libpath)
 }
